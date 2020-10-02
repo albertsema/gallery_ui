eed9b55374e2a000dee42595b92a3c6018b2ed22
Albert Matabaro <sema@hey.com>
2020-10-02 13:49:04 -0400

    Keep gallery in the backgroud when a photo is clicked on

--- src/views/Photo.vue
+++ src/views/Photo.vue
@@ -1,9 +1,17 @@
 <template>
-  <Photo />
-</template><script>
-import Photo from '@/components/Photo.vue';export default {
+  <div>
+    <Gallery />
+    <Photo />
+  </div>
+</template>
+<script>
+import Gallery from '@/components/Gallery.vue';
+import Photo from '@/components/Photo.vue';
+
+export default {
   name: 'photo',
   components: {
+    Gallery,
     Photo,
   },
 };
