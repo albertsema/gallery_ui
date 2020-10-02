49cd67d7683fd2fb2972661b5a5cb933e50159cd
Albert Sema <sema@hey.com>
2020-10-02 13:39:55 -0400

    app setup, router, home component

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Photo from '../views/Photo.vue';
 
 const routes = [
   {
@@ -8,13 +9,10 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+      path: '/photo/:id',
+      name: 'photo',
+      component: Photo,
+  },
 ]
 
 const router = createRouter({
